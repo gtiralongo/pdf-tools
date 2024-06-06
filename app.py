87f5609ce5b2add23dce2b44aec7cef7d27e8aa1
GT <55749965+gtiralongo@users.noreply.github.com>
2024-06-06 15:34:34 -0300

    vuelta al estable

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from PyPDF2 import PdfMerger, PdfReader, PdfWriter
-import ghostscript
-import os
 
 def merge_pdfs(pdf_files):
     merger = PdfMerger()
@@ -13,21 +11,16 @@ def merge_pdfs(pdf_files):
         merger.write(f)
     return merged_pdf_path
 
-def compress_pdf(input_pdf_path, output_pdf_path):
-    args = [
-        "gs",
-        "-sDEVICE=pdfwrite",
-        "-dCompatibilityLevel=1.4",
-        "-dPDFSETTINGS=/screen",  # Cambia a /ebook, /printer o /prepress para diferentes niveles de compresión
-        "-dNOPAUSE",
-        "-dQUIET",
-        "-dBATCH",
-        f"-sOutputFile={output_pdf_path}",
-        input_pdf_path,
-    ]
-
-    ghostscript.Ghostscript(*args)
-    return output_pdf_path
+def compress_pdf(pdf_file):
+    reader = PdfReader(pdf_file)
+    writer = PdfWriter()
+    for i in range(len(reader.pages)):
+        writer.add_page(reader.pages[i])
+    
+    compressed_pdf_path = "compressed.pdf"
+    with open(compressed_pdf_path, "wb") as f:
+        writer.write(f)
+    return compressed_pdf_path
 
 def main():
     st.title("Herramienta para unir y comprimir PDFs")
@@ -47,13 +40,10 @@ def main():
     uploaded_file = st.file_uploader("Sube tu PDF para comprimir", type=["pdf"], key="compress")
     if st.button("Comprimir PDF"):
         if uploaded_file:
-            with open("temp_uploaded.pdf", "wb") as f:
-                f.write(uploaded_file.read())
-            compressed_pdf = compress_pdf("temp_uploaded.pdf", "compressed.pdf")
+            compressed_pdf = compress_pdf(uploaded_file)
             st.success("PDF comprimido correctamente!")
             with open(compressed_pdf, "rb") as f:
                 st.download_button(label="Descargar PDF comprimido", data=f, file_name="compressed.pdf", mime="application/pdf")
-            os.remove("temp_uploaded.pdf")  # Limpiar el archivo temporal
         else:
             st.error("Sube un archivo PDF.")
     
